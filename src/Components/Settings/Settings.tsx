6010a3bb4979644dcffc8d60fa64b187ad2f6927
petritsyn <andrey2343@yandex.ru>
2022-10-27 04:04:35 +0300

    disable buttons

--- src/Components/Settings/Settings.tsx
+++ src/Components/Settings/Settings.tsx
@@ -1,16 +1,15 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent} from 'react';
 import {Button} from "../Button/Button";
 import s from './Settings.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../redux/store";
-import {onMaxValueChange} from "../../redux/SettingsReducer";
+import {onMaxValueChange, onStartValueChange} from "../../redux/SettingsReducer";
 
 export const Settings = () => {
 
-    let [maxInputValue, setMaxInputValue] = useState(0)
-
     let maxValue = useSelector<AppRootStateType, number>(state => state.settings.maxValue)
-    let isMaxValueChange = useSelector<AppRootStateType, boolean>(state => state.settings.isMaxValueChange)
+    let startValue = useSelector<AppRootStateType, number>(state => state.settings.startValue)
+    let disableButton = useSelector<AppRootStateType, boolean>(state => state.settings.isDisableButton)
 
     let dispatch = useDispatch()
 
@@ -18,21 +17,24 @@ export const Settings = () => {
         console.log('set')
     }
 
-    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setMaxInputValue(+e.currentTarget.value)
+    const onChangeInputMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
         dispatch(onMaxValueChange(+e.currentTarget.value))
+    }
+
+    const onChangeInputStartValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch(onStartValueChange(+e.currentTarget.value))
     }
 
     return (
         <div className='appItem'>
             <div className='displayField'>
                 <div className={s.settings}>
-                    <div><span>max value:</span><input type="number" value={maxValue} onChange={onChangeInputHandler}/></div>
-                    <div><span>start value:</span><input type="number"/></div>
+                    <div><span>max value:</span><input type="number" value={maxValue} onChange={onChangeInputMaxValueHandler}/></div>
+                    <div><span>start value:</span><input type="number" value={startValue} onChange={onChangeInputStartValueHandler}/></div>
                 </div>
             </div>
             <div className='buttonsField'>
-                <Button name={'set'} onClickButtonHandler={onClickButtonHandler} isDisabled={isMaxValueChange}/>
+                <Button name={'set'} onClickButtonHandler={onClickButtonHandler} isDisabled={!disableButton}/>
             </div>
         </div>
     );
--- src/redux/SettingsReducer.ts
+++ src/redux/SettingsReducer.ts
@@ -1,13 +1,15 @@
 type InitialStateType = {
-    isMaxValueChange: boolean
+    isDisableButton: boolean
     maxValue: number
+    startValue: number
 }
 
-type ActionsTypes = ReturnType<typeof onMaxValueChange>
+type ActionsTypes = ReturnType<typeof onMaxValueChange> | ReturnType<typeof onStartValueChange>
 
 const initialState = {
-    isMaxValueChange: false,
-    maxValue: 0
+    isDisableButton: false,
+    maxValue: 0,
+    startValue: 0
 }
 
 export const SettingsReducer = (state: InitialStateType = initialState, action: ActionsTypes) => {
@@ -17,7 +19,15 @@ export const SettingsReducer = (state: InitialStateType = initialState, action: 
             return {
                 ...state,
                 maxValue: action.maxValue,
-                isMaxValueChange: true
+                isDisableButton: true
+            }
+        }
+
+        case "ON-START-VALUE-CHANGE": {
+            return {
+                ...state,
+                startValue: action.startValue,
+                isDisableButton: true
             }
         }
 
@@ -30,5 +40,12 @@ export const onMaxValueChange = (maxValue: number) => {
     return {
         type: 'ON-MAX-VALUE-CHANGE',
         maxValue
+    } as const
+}
+
+export const onStartValueChange = (startValue: number) => {
+    return {
+        type: 'ON-START-VALUE-CHANGE',
+        startValue
     } as const
 }
